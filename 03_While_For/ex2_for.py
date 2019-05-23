c8eba6efe4de22d4fc366df4fc73550c7eee6da8
AndreyIvantsov <tetranod@mail.ru>
2019-05-24 02:33:58 +0300

    Обновлены и добавлены некоторые файлы

--- 03_While_For/ex2_for.py
+++ 03_While_For/ex2_for.py
@@ -5,12 +5,22 @@
 # //                      Цикл for
 # //
 
-word = input('Введете строку: ')
-
-for lettet in word:
-    print("{}.".format(lettet), end='')
+HEADLINE: str = ' Цикл for '
 
 print('\n')
+print(HEADLINE.center(40, '*'), '\n')
+
+
+# Обход строки
+
+word = input('Введете строку: ')
+
+for letter in word:
+    print("{}.".format(letter), end='')
+
+print('\n')
+
+# Функция range()
 
 for i in range(10):
     print(i, end=' ')
@@ -31,3 +41,29 @@ for i in range(5, 30, 3):
     print(i, end=' ')
 
 print('\n')
+
+# Обход кортежа
+
+colors = ('red', 'yellow', 'green',)  # Кортеж
+
+for color in colors:
+    print(color)
+
+print('\n')
+
+#  Выборка одинаковых элементов
+
+lst_1 = ['Sem', 'Jon', 235, ('3', 3), 3.14, 555, 'Red']
+
+lst_2 = [777, 'Sem',  ('3', 3), 3.14,  'Green', 'Jon']
+
+selection = []
+
+for item in lst_1:
+    if item in lst_2:
+        selection.append(item)
+
+print('Первый список       :', lst_1)
+print('Второй список       :', lst_2)
+print('Одинаковые эметенты :', selection)
+
